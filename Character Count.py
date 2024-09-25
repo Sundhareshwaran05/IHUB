3fe3c908bab7e6467cf68ea99d8c4dfb2d393c38
Sundhareshwaran R <94157333+Sundhareshwaran05@users.noreply.github.com>
2024-09-25 13:16:20 +0530

    first commit

---
+++ Character Count.py
@@ -0,0 +1,12 @@
+input1 = input()
+input2 = int(input())
+input3 = input()
+a = list(input1)
+b = []
+for i in a:
+    if input3 == i:
+        b.append(i)
+    else :
+        continue
+print(b)
+print(len(b))
